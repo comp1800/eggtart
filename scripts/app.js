6d8f553fe9fb646e29c1dc3ebe0c075fee229782
Carly Orr <carly_wong_orr@bcit.ca>
2020-11-03 10:14:15 -0800

    adding firestore read demo

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
 
+function readQuote(){
+    db.collection("quotes").doc("01")
+    .onSnapshot(function(snap){
+        console.log(snap.data());   //print the document fields of "01"
+        console.log(snap.data().message);
+        document.getElementById("abc").innerText = snap.data().message;
+    })
+}
+readQuote();
